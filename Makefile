b9159b36ac3bd7db341455c89f09ba9fd4f3804c
Ivan Kapelyukhin <ikapelyukhin@users.noreply.github.com>
2018-08-25 17:48:15 +0200

    Integrate with xscreensaver

--- Makefile
+++ Makefile
@@ -1,4 +1,3 @@
 all:
 	g++ -c main.cpp -I. -I./nlohmann/json/single_include
-	g++ main.o -o geekofall -lsfml-graphics -lsfml-window -lsfml-system -lBox2D
-
+	g++ main.o -o geekofall -lsfml-graphics -lsfml-window -lsfml-system -lBox2D -lX11
--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <SFML/Graphics.hpp>
-#include <SFML/Window/Mouse.hpp>
 #include <Box2D/Box2D.h>
+#include <X11/Xlib.h>
 #include <nlohmann/json.hpp>
 #include <iostream>
 #include <fstream>
@@ -16,107 +16,137 @@ class Geekofall {
   sf::RenderWindow* window;
   b2World* world;
   sf::Texture geekoTexture;
-  
-  int ticks = 0;
-  
-  public:
-  
-  void createSFMLWindow() {
-    sf::ContextSettings settings;
-    settings.antialiasingLevel = 8;
 
-    window = new sf::RenderWindow(sf::VideoMode(800, 600, 32), "Geekofall", sf::Style::Default, settings);
+  int ticks = 0;
+
+  public:
+
+  void createSFMLWindow() {
+    char* wid_env;
+
+    if ((wid_env = getenv("XSCREENSAVER_WINDOW")) != NULL) {
+      int width, height;
+      Display *display;
+
+      if ((display = XOpenDisplay(NULL)) == NULL) exit(1);
+
+      Window x11win;
+      x11win = (Window) strtol(wid_env, (char **) NULL, 0);
+
+      int screen = DefaultScreen(display);
+      XWindowAttributes windowAttributes;
+
+      XGetWindowAttributes(display, x11win, &windowAttributes);
+			width = windowAttributes.width;
+			height = windowAttributes.height;
+
+      Window x11view = XCreateWindow(
+        display, x11win,
+        0, 0, width, height, 0,
+        DefaultDepth(display, screen), InputOutput,
+        DefaultVisual(display, screen), 0, NULL
+      );
+
+      XMapWindow(display, x11win);
+      XFlush(display);
+
+      window = new sf::RenderWindow(x11view);
+    } else {
+      sf::ContextSettings settings;
+      settings.antialiasingLevel = 8;
+      window = new sf::RenderWindow(sf::VideoMode(800, 600, 32), "Geekofall", sf::Style::Default, settings);
+    }
     window->setFramerateLimit(60);
   }
-  
+
   void createBox2DWorld() {
     b2Vec2 gravity(0.f, 9.8f);
     world = new b2World(gravity);
   }
-  
+
   b2Body* createShelf() {
     b2BodyDef bodyDef;
     bodyDef.position = b2Vec2(4, 6);
     bodyDef.type = b2_staticBody;
     b2Body* body = world->CreateBody(&bodyDef);
-    
+
     b2PolygonShape shape;
     shape.SetAsBox(5, 0.025f);
     b2FixtureDef fixtureDef;
     fixtureDef.density = 0;
     fixtureDef.shape = &shape;
     body->CreateFixture(&fixtureDef);
-    
+
     return body;
   }
-  
+
   b2Body* createGeeko(int x, int y) {
     b2BodyDef bodyDef;
     bodyDef.position = b2Vec2(3, 0);
     bodyDef.type = b2_dynamicBody;
     b2Body* body = world->CreateBody(&bodyDef);
-    
+
     auto polygons = json_data["rigidBodies"][0]["polygons"];
     for (auto iter = polygons.begin(); iter != polygons.end(); iter++) {
       auto vertices = iter.value();
-      
+
       b2PolygonShape polygonShape;
       b2Vec2 *b2vertices = new b2Vec2[vertices.size()];
-    
+
       for (auto vertex_iter = vertices.begin(); vertex_iter != vertices.end(); vertex_iter++) {
         auto vertex = vertex_iter.value();
         int idx = std::distance(vertices.begin(), vertex_iter);
-        
+
         b2vertices[idx].Set((double)vertex["x"], -(double)vertex["y"]);
       }
-      
+
       polygonShape.Set(b2vertices, vertices.size());
-      
+
       /*****/
 
       b2FixtureDef fixtureDef;
       fixtureDef.shape = &polygonShape;
       fixtureDef.density = 1;
-      fixtureDef.restitution = 0.3f;
+      fixtureDef.restitution = 0.9f;
 
       body->CreateFixture(&fixtureDef);
     }
-    
+
     return body;
   }
-  
+
   void drawBodyMesh(b2Body* body) {
     b2Vec2 pos = body->GetPosition();
-    
+
     for (b2Fixture* f = body->GetFixtureList(); f; f = f->GetNext()) {
       b2PolygonShape* polygonShape = (b2PolygonShape*)f->GetShape();
-      
+
       int count = polygonShape->GetVertexCount();
-      
+
       sf::ConvexShape convex;
       convex.setPointCount(count);
 
       for(int i = 0; i < count; i++) {
         b2Vec2 vert = body->GetWorldPoint(polygonShape->GetVertex(i));
-        
+
         convex.setPoint(i, sf::Vector2f(vert.x * 100, vert.y * 100));
       }
-      
+
       convex.setOutlineThickness(1);
       convex.setOutlineColor(sf::Color(250, 150, 100));
       convex.setFillColor(sf::Color::Black);
       window->draw(convex);
-      
+
       sf::CircleShape shape(3);
       shape.setPosition(pos.x*100, pos.y*100);
       shape.setFillColor(sf::Color(100, 250, 50));
       window->draw(shape);
     }
   }
-  
+
   void drawBodySprite(b2Body* body) {
     b2Vec2 pos = body->GetPosition();
-    
+
     sf::Sprite sprite;
     sprite.setTexture(geekoTexture);
     sprite.setOrigin(0, 82);
@@ -125,19 +155,19 @@ class Geekofall {
     sprite.rotate(body->GetAngle()*180/b2_pi);
     window->draw(sprite);
   }
-  
+
   Geekofall() {
-    std::ifstream ifs("geeko.json");
+    std::ifstream ifs("geeko.json"); // FIXME: path in the Makefile
     json_data = json::parse(ifs);
-    
+
     createSFMLWindow();
     createBox2DWorld();
-    
+
     auto shelf = createShelf();
-    
-    geekoTexture.loadFromFile("geeko.png");
+
+    geekoTexture.loadFromFile("geeko.png"); // FIXME: path in the Makefile
     geekoTexture.setSmooth(true);
-    
+
     while (window->isOpen()) {
       sf::Event event;
       while (window->pollEvent(event)) {
@@ -152,22 +182,22 @@ class Geekofall {
           int MouseY = sf::Mouse::getPosition(Window).y;
       }
       */
-      
+
       if (ticks == 0) createGeeko(3,2);
-      
+
       window->clear(sf::Color::Black);
-      
+
       //drawBodyMesh(shelf);
-      
+
       for (b2Body* body = world->GetBodyList(); body != 0; body = body->GetNext()) {
         if (body->GetType() == b2_dynamicBody) {
           //drawBodyMesh(body);
           drawBodySprite(body);
         }
       }
-      
+
       window->display();
-      
+
       ticks = (ticks + 1) % 60;
       world->Step(1/60.f, 8, 3);
     }
